51820ed79946207a843a447d91f0c81bc594e445
Hadassah Uche <uhadassah1@gmail.com>
2023-11-16 11:16:56 +0200

    copied the program from slide 13

--- UcheHadassah6_2/HadassahUche6_2.cpp
+++ UcheHadassah6_2/HadassahUche6_2.cpp
@@ -4,3 +4,21 @@ and one character and returns the number of times the character occurs in the st
 Write a program to test your function with user-defined data.
 For example: Assuming the string passed is “Hello” and the letter is “l”, your 
 function should return 2.*/
+#include <iostream> 
+//Counts the number of times the letter h appears in string
+using namespace std;
+int main()
+{
+	int i, count = 0;
+	char word[15];
+	cout << "Type in a word: ";
+	cin >> word;
+	for (i = 0; word[i] != '\0'; i++)
+		if (word[i] == 'h')
+			count++;
+	if (count == 1)
+		cout << "The letter h appears 1 time" << endl;
+	else
+		cout << "The letter h appears " << count << "times" << endl;
+	return 0;
+}
