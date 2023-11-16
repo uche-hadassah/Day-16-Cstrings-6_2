d56e55ac88a8d2f4c8c0212ae90ab40d96c84578
Hadassah Uche <uhadassah1@gmail.com>
2023-11-16 11:07:34 +0200

    Add project files.

---
+++ UcheHadassah6_2/HadassahUche6_2.cpp
@@ -0,0 +1,6 @@
+/*Counting occurrences: Re-write the program given in slide 13 (Topic C-Strings) 
+using a function: Write a function called Count which takes as parameters a string 
+and one character and returns the number of times the character occurs in the string. 
+Write a program to test your function with user-defined data.
+For example: Assuming the string passed is “Hello” and the letter is “l”, your 
+function should return 2.*/
